5a9edf65e8268c308b393c753e3b53d2d9e1f7ee
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-03-08 11:25:32 +0100

    typescript: import / export

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -1,22 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { Test } from './test';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-
-
-
-class Test {
-
-  constructor(public zahl: number = 0) {
-    console.error('Hallo Welt!! 😀', this.zahl)
-  }
-
-  public blubb() { }
-}
 
 
 const test = new Test(42);
---
+++ book-rating/src/test.ts
@@ -0,0 +1,11 @@
+
+
+export class Test {
+
+  constructor(public zahl: number = 0) {
+    console.error('Hallo Welt!! 😀', this.zahl)
+  }
+
+  public blubb() { }
+}
+
