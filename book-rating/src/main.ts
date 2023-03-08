f92d27d0ba60fae4a4ae1dd7f9fce2b63d66a2da
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-03-08 10:52:54 +0100

    typescript: Klassen

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -5,3 +5,19 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+
+
+class Test {
+
+  constructor(zahl: number) {
+    console.error('Hallo Welt!! 😀', zahl)
+  }
+
+  public blubb() { }
+}
+
+
+const test = new Test(42);
+test.blubb();
