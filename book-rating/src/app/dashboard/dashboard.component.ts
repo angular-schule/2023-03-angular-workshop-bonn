a79bada6e55e797787b73ad9574c4c26d8e5dfb0
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-03-09 09:03:19 +0100

    tipp: ngOnInit ist KEINE gute Praxis mehr (passt nicht gut zum strict mode)

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-dashboard',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  books: Book[];
+
+  ngOnInit() {
+    this.books = [];
+  }
 }
