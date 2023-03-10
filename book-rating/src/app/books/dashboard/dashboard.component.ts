0b62d098b1f89c3eb8288f6f7b1ae4ac22986ffa
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-03-10 14:12:08 +0100

    updateAndSort Books

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 @Component({
   selector: 'br-dashboard',
@@ -24,4 +25,22 @@ export class DashboardComponent {
     description: 'zu alt',
     rating: 1
   }];
+
+  constructor(private br: BookRatingService) { }
+
+  doRateUp(book: Book): void {
+    const ratedBook = this.br.rateUp(book);
+    this.updateAndSort(ratedBook);
+  }
+
+  doRateDown(book: Book): void {
+    const ratedBook = this.br.rateUp(book);
+    this.updateAndSort(ratedBook);
+  }
+
+  updateAndSort(ratedBook: Book): void {
+    this.books = this.books
+      .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
+      .sort((a, b) => b.rating - a.rating)
+  }
 }
