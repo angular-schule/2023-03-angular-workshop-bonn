fc15d83d8a196b309b2a1a5a2c21380c6e7c85a6
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-03-13 10:10:03 +0100

    testing behaviour

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BookComponent } from '../book/book.component';
+import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 import { DashboardComponent } from './dashboard.component';
 
@@ -7,8 +10,20 @@ describe('DashboardComponent', () => {
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async () => {
+
+    const bookRatingMock = {
+      rateUp: (book: Book) => book
+    }
+
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [
+        DashboardComponent,
+        BookComponent // Integration Test
+      ],
+      providers: [{
+        provide: BookRatingService,
+        useValue: bookRatingMock
+      }]
     })
     .compileComponents();
 
@@ -17,7 +32,14 @@ describe('DashboardComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('doRateUp() should forward all calls to BookRatingService', () => {
+
+    const bookRatingMock = TestBed.inject(BookRatingService);
+    spyOn(bookRatingMock, 'rateUp').and.callThrough();
+
+    const book = { } as Book;
+    component.doRateUp(book);
+
+    expect(bookRatingMock.rateUp).toHaveBeenCalledOnceWith(book);
   });
 });
