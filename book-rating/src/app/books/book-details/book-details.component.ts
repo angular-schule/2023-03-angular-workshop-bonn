832a19a11b31572673294a0b892d7ea1f7fee613
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-03-15 11:43:00 +0100

    rxjs: anti-pattern, doppelt subscribe

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-book-details',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-details.component.scss']
 })
 export class BookDetailsComponent {
+
+  bs = inject(BookStoreService);
+
+  book$ = inject(ActivatedRoute).paramMap.pipe(
+    map(paramMap => paramMap.get('isbn') || ''),
+    map(isbn => this.bs.getSingleBook(isbn))
+  )
+
+  // Option A, damit es mit useDefineForClassFields funktioniert
+  // $book$: Observable<any>;
+  // constructor(route: ActivatedRoute) {
+  //   this.$book$ = route.paramMap
+  // }
 
 }
